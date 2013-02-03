08b829854a0be782b0bad4d7943ed757d84b41f2
Eric Link <eric@ericmlink.com>
2013-02-03 14:07:31 -0600

    basic faq using default service

--- app/js/services.js
+++ app/js/services.js
@@ -7,22 +7,26 @@
 angular.module('myApp.services', []).value('version', '0.2');
 
 angular.module('myApp.faq', ['ngResource']).factory('Faq', function($resource) {
+   return $resource('faqs:id.json', {} );
+});
+
+
 	//return $resource('faqs.json');
 	//return $resource('phones/:phoneId.json', {}, {
-	var Faq = $resource('faqs:id.json', {}, {
-		query: {method: 'GET', params: {faqId: 'faqs'}, isArray: true},
-		get: {method: 'GET', params: {faqId: 'faqs'}, isArray: false}
-	});
-
-	Faq.prototype.update = function(cb) {
-		return Faq.update({id: this.id},
-		angular.extend({}, this, {id: undefined}), cb);
-	};
-
-	Faq.prototype.destroy = function(cb) {
-		return Faq.remove({id: this.id}, cb);
-	};
-
-	return Faq;
-});
+//	var Faq = $resource('faqs:id.json', {}, {
+//		query: {method: 'GET', params: {faqId: 'faqs'}, isArray: true},
+//		get: {method: 'GET', params: {faqId: 'faqs'}, isArray: false}
+//	});
+//
+//	Faq.prototype.update = function(cb) {
+//		return Faq.update({id: this.id},
+//		angular.extend({}, this, {id: undefined}), cb);
+//	};
+//
+//	Faq.prototype.destroy = function(cb) {
+//		return Faq.remove({id: this.id}, cb);
+//	};
+//
+//	return Faq;
+//});
 
