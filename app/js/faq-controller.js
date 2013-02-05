03531fdec20985a5b3ad327fa2e054994c46c801
Eric Link <eric@ericmlink.com>
2013-02-04 20:39:20 -0600

    Clean CRUD

--- app/js/faq-controller.js
+++ app/js/faq-controller.js
@@ -21,18 +21,16 @@ function FaqCtrlEdit($scope, $location, $routeParams, Faq) {
 
     $scope.destroy = function() {
         console.log('destroy');
-        Faq.delete({id: $routeParams.faqId});
-        $location.path('/faq');
-// fixme callback?
-//        self.original.destroy(function() {
-//            $location.path('/faq');
-//        });
+        Faq.delete({id: $routeParams.faqId}, function() {
+            $location.path('/faq');
+        });
     };
     $scope.save = function() {
         console.log('save');
         console.log($scope.faq);
-        Faq.save($scope.faq);
-        $location.path('/faq');
+        Faq.save($scope.faq, function() {
+            $location.path('/faq');
+        });
     };
 }
 
@@ -40,8 +38,10 @@ function FaqCtrlNew($scope, $location, Faq) {
     console.log('faq new');
     $scope.save = function() {
         console.log('save');
-        Faq.save($scope.faq);
-        $location.path('/faq');
+        Faq.save($scope.faq, function() {
+            $location.path('/faq');
+            //$scope.apply();
+        });
     };
 }
 
