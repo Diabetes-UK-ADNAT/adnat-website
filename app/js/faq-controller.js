0cf5795ea21c0dd4b057178308b4f96103f62913
Eric Link <eric@ericmlink.com>
2013-02-10 16:53:55 -0600

    basic checkbox list working for all cases

--- app/js/faq-controller.js
+++ app/js/faq-controller.js
@@ -6,8 +6,6 @@ function FaqCtrl($scope, Faq) {
 }
 
 function FaqCtrlEdit($scope, $location, $routeParams, Faq) {
-    console.log('faq edit');
-    console.log($routeParams.faqId);
     var self = this;
 
     $scope.categoryOptions = CategoryOptions();
@@ -15,37 +13,32 @@ function FaqCtrlEdit($scope, $location, $routeParams, Faq) {
         self.original = faq;
         $scope.categoryChoices = [];
         angular.forEach($scope.categoryOptions, function(value, key) {
-            console.log('key'+key+ ' value ' + value + ' ' + faq.categories.indexOf(value))
-            if (faq.categories.indexOf(value) > -1 ) {
+            if (faq.categories.indexOf(value) > -1) {
                 $scope.categoryChoices[key] = true;
             } else {
                 $scope.categoryChoices[key] = false;
             }
         });
+
+        $scope.categoryChoicesOriginal = [];
+        angular.copy($scope.categoryChoices, $scope.categoryChoicesOriginal);
         $scope.faq = new Faq(self.original);
     });
 
     $scope.isClean = function() {
         return angular.equals(self.original, $scope.faq)
-                && angular.equals(self.categoryChoices, $scope.categoryChoices)
-                ;
+            &&
+            angular.equals($scope.categoryChoices, $scope.categoryChoicesOriginal)
+            ;
     };
 
     $scope.destroy = function() {
-        console.log('destroy');
         Faq.delete({id: $routeParams.faqId}, function() {
             $location.path('/faq');
         });
     };
     $scope.save = function() {
-        console.log('save');
-        console.log($scope.faq);
-        console.log($scope.categoryChoices);
-
-        //save category choices
-        console.log($scope.faq.categories);
         $scope.faq.categories.length = 0;
-        console.log($scope.faq.categories);
         angular.forEach($scope.categoryChoices, function(value, key) {
             if (value) {
                 $scope.faq.categories.push($scope.categoryOptions[key]);
@@ -59,11 +52,16 @@ function FaqCtrlEdit($scope, $location, $routeParams, Faq) {
 }
 
 function FaqCtrlNew($scope, $location, Faq) {
-    console.log('faq new');
     $scope.categoryOptions = CategoryOptions();
     $scope.categoryChoices = [];
     $scope.save = function() {
-        console.log('save');
+        $scope.faq.categories = [];
+        angular.forEach($scope.categoryChoices, function(value, key) {
+            if (value) {
+                $scope.faq.categories.push($scope.categoryOptions[key]);
+            }
+        });
+
         Faq.save($scope.faq, function() {
             $location.path('/faq');
         });
