4458406f014956e4c1ba86f105a6f42371d7d491
Eric Link <eric@ericmlink.com>
2013-05-24 23:52:36 -0500

    config urls

--- prepush.sh
+++ prepush.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
-date
-time find . -type f -name "*.js" |xargs grep "IS_DEV"
-date
-
-echo "Look for isDev: true - set to false"
+find . -type f -name "*.js" |xargs grep --color "ConfigIsDev ="
