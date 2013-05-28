5e38f5e7644319c4a8f9a7d21a19a5b228348ae7
Eric Link <eric@ericmlink.com>
2013-05-27 23:21:47 -0500

    @Restrict all public controller methods

--- app/js/page-controller.js
+++ app/js/page-controller.js
@@ -1,5 +1,6 @@
 'use strict';
-function PageCtrl($scope, $routeParams, Content) {
+function PageCtrl($scope, $routeParams, Content, $http) {
+	$http.defaults.headers.common['X-App-Key'] = "13B6EFE5-63EE-4F1C-A486-76B24AAE1704";
 	$scope.pages = Content.query(
 			function() {
 			},
@@ -7,7 +8,8 @@ function PageCtrl($scope, $routeParams, Content) {
 				toastr.error('Error loading data');
 			});
 }
-function PageFindCtrl($scope, $routeParams, Content) {
+function PageFindCtrl($scope, $routeParams, Content, $http) {
+	$http.defaults.headers.common['X-App-Key'] = "13B6EFE5-63EE-4F1C-A486-76B24AAE1704";
 	var ids = $routeParams.id.split(',');
 	$scope.content = [];
 	ids.forEach(function(item) {
@@ -20,10 +22,9 @@ function PageCtrlEdit($scope, $routeParams, Content, $http, $cookies, $location)
 		window.location = Config.urlLogin;
 		return;
 	}
-	// must encodeURI for FireFox or get an error alert
+// must encodeURI for FireFox or get an error alert
 	$http.defaults.headers.common['X-Auth-Token'] = encodeURI($cookies.aut);
 	$http.defaults.headers.common['X-App-Key'] = "13B6EFE5-63EE-4F1C-A486-76B24AAE1704";
-
 	var self = this;
 	$scope.content = Content.get({id: $routeParams.id}, function(content) {
 		self.original = content;
@@ -32,11 +33,9 @@ function PageCtrlEdit($scope, $routeParams, Content, $http, $cookies, $location)
 			function() {
 				toastr.error('Error loading data');
 			});
-
 	$scope.isClean = function() {
 		return angular.equals(self.original, $scope.content);
 	};
-
 	$scope.destroy = function() {
 		Content.delete({id: $routeParams.id},
 		function() {
@@ -65,10 +64,9 @@ function PageCtrlNew($scope, Content, $http, $cookies, $location) {
 		window.location = Config.urlLogin;
 		return;
 	}
-	// must encodeURI for FireFox or get an error alert
+// must encodeURI for FireFox or get an error alert
 	$http.defaults.headers.common['X-Auth-Token'] = encodeURI($cookies.aut);
 	$http.defaults.headers.common['X-App-Key'] = "13B6EFE5-63EE-4F1C-A486-76B24AAE1704";
-
 	$scope.content = new Content();
 	$scope.save = function() {
 		Content.save($scope.content,
