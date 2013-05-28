b6859301ae3a0821587bef3585f137d3bf414741
Eric Link <eric@ericmlink.com>
2013-05-27 23:54:37 -0500

    prod

--- app/js/config.js
+++ app/js/config.js
@@ -1,6 +1,6 @@
-var ConfigIsDev = true;
+var ConfigIsDev = false;
 var Config = {
-	version: '2.1.71',
+	version: '2.1.72',
 	urlServicesRoot: ConfigIsDev ? 'https://api.myadnat.co.uk\\:4443/v1' : 'https://api.myadnat.co.uk\\:443/v1',
 	urlPersons: ConfigIsDev ? 'https://api.myadnat.co.uk:4443/v1/persons' : 'https://api.myadnat.co.uk/v1/persons',
 	urlLogin: ConfigIsDev ? 'https://auth.myadnat.co.uk:4443/login' : 'https://auth.myadnat.co.uk/login',
