e5ac46b7f6230fa4e708321c9c15497d28cea995
Eric Link <eric@ericmlink.com>
2013-05-27 22:24:04 -0500

    subject

--- app/js/config.js
+++ app/js/config.js
@@ -5,6 +5,7 @@ var Config = {
 	urlPersons: ConfigIsDev ? 'https://api.myadnat.co.uk:4443/v1/persons' : 'https://api.myadnat.co.uk/v1/persons',
 	urlLogin: ConfigIsDev ? 'https://auth.myadnat.co.uk:4443/login' : 'https://auth.myadnat.co.uk/login',
 	urlLogout: ConfigIsDev ? 'https://auth.myadnat.co.uk:4443/logout' : 'https://auth.myadnat.co.uk/logout',
-	urlProfile: ConfigIsDev ? 'https://auth.myadnat.co.uk:4443/profile' : 'https://auth.myadnat.co.uk/profile'
+	urlProfile: ConfigIsDev ? 'https://auth.myadnat.co.uk:4443/profile' : 'https://auth.myadnat.co.uk/profile',
+	urlSubject: ConfigIsDev ? 'https://auth.myadnat.co.uk:4443/v1/subject' : 'https://auth.myadnat.co.uk/v1/subject'
 };
 
--- app/js/dashboard-controller.js
+++ app/js/dashboard-controller.js
@@ -2,22 +2,30 @@
 function DashboardCtrl($scope, $routeParams, Person, $http, $cookies, $location) {
 	// FIXME make auth service to guard no public functions ($cookies, $http)
 	if (typeof $cookies.aut === 'undefined' || $cookies.aut.indexOf('pa.u.id') === -1 && $cookies.aut.indexOf('pa.u.exp') === -1 && $cookies.aut.indexOf('pa.p.id') === -1) {
-		window.location = Config.urlLogin; 
+		window.location = Config.urlLogin;
 		return;
 	}
 	// must encodeURI for FireFox or get an error alert
 	$http.defaults.headers.common['X-Auth-Token'] = encodeURI($cookies.aut);
 	$http.defaults.headers.common['X-App-Key'] = "13B6EFE5-63EE-4F1C-A486-76B24AAE1704";
-	// 
-//	// +Role checks
-//	$scope.persons = Person.query(
-//			function() {
-//			},
-//			function() {
-//				toastr.error('Error loading data');
-//			}
-//	);
 
+	// auth subject
+	$http.get(Config.urlSubject).then(function(response) {
+		$scope.subject = response.data;
+	}, function(error) {
+	});
+	$scope.hasRole = function(role) {
+		// must use flag because returning from foreach just does continue on loop. 
+		var found = false;
+		if ($scope.subject) {
+			angular.forEach($scope.subject.roles, function(value, key) {
+				if (value === role) {
+					found = true;
+				}
+			});
+		}
+		return found;
+	};
 	$http.get(Config.urlPersons, {
 		params: {qRole: 'Patient'}
 	}).then(function(response) {
@@ -37,7 +45,6 @@ function DashboardCtrl($scope, $routeParams, Person, $http, $cookies, $location)
 	});
 	$scope.roles = RoleOptions();
 	$scope.role = $routeParams.role;
-
 }
 
 
