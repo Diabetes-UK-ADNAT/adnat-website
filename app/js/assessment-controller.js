b3aa43561ed688d853d65a48dc49fecaa01c4e24
Eric Link <eric@ericmlink.com>
2013-10-26 14:18:12 -0500

    Include all scored questions regardless of type

--- app/js/assessment-controller.js
+++ app/js/assessment-controller.js
@@ -37,8 +37,12 @@ function AssessmentCtrlDetail($scope, $routeParams, Assessment, $http, $cookies,
 	};
 
 	$scope.isScoringQuestionFilter = function(response) {
-		return !$scope.filterOnScoringQuestions || response.type === 'SC-SL';
-	}
+		return !$scope.filterOnScoringQuestions   //filter is not active so return all
+				|| response.type === 'SC-SL' // stoplight question types are all scored (psychosocial)
+				// include all scored regardless of type
+				|| AssessmentControllerHelper.arrayHasVal(AssessmentControllerHelper.scoredQuestionIds(), response.q)
+				;
+	};
 
 	$scope.assessment = Assessment.get({id: $routeParams.id}, function() {
 		$scope.updated = new Date($scope.assessment.updated).toString();
@@ -60,6 +64,19 @@ function AssessmentCtrlDetail($scope, $routeParams, Assessment, $http, $cookies,
 }
 
 var AssessmentControllerHelper = {
+	'scoredQuestionIds': function() {
+		// include all scored questions regardless of type.  source is touch app scoreconfigs.json q=int
+		return [10, 11, 16, 18, 19, 20, 21, 22, 28, 29, 34, 35, 38, 41, 45, 48, 49, 50, 51, 53, 55, 56, 57, 66, 76, 77, 78, 80, 90, 91, 92, 94, 111, 112, 113, 117];
+	},
+	'arrayHasVal': function(arr, val) {
+		for (var i = 0; arr !== null && i < arr.length; i++) {
+			if (arr[i] === val) {
+				return true;
+			}
+		}
+		;
+		return false;
+	},
 	'categoryOptions': function() {
 		return [
 			"About You",
